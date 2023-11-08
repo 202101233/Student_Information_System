69fa7cccbbabc59e4ae1c5ef0138715ee327acbc
Sagar-008 <139506515+Sagar-008@users.noreply.github.com>
2023-11-08 18:31:24 +0000

    Latest Controller and Connection files

--- SW_Project/Server/Database/connection.js
+++ SW_Project/Server/Database/connection.js
@@ -2,12 +2,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
     try {
-<<<<<<< HEAD
-        const con = await mongoose.connect("mongodb://127.0.0.1:27017/admin", {
-=======
-
         const con = await mongoose.connect("mongodb+srv://admin:sis_it304@cluster0.7fcnw1p.mongodb.net/?retryWrites=true&w=majority", {
->>>>>>> origin/main
             useNewUrlParser: true,
             useUnifiedTopology : true,
             // useFindAndModify : false,
